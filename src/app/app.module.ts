62d2d4cbe37214533229ed927f32be19615946ec
Dominique Dresen <dominique.dresen@uni-koeln.de>
2018-03-15 22:02:50 +0100

    Added basic packages and setup routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { AppRoutingModule } from './app-routing.module';
+import { SwaptrackerModule } from './swaptracker/swaptracker.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    SwaptrackerModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/primeng/primeng.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { ButtonModule, ContextMenuModule, DialogModule,
+         FileUploadModule, InputTextareaModule, GrowlModule,
+         PanelModule, TooltipModule } from 'primeng/primeng';
+
+
+@NgModule({
+  imports: [],
+  exports: [
+    ButtonModule,
+    ContextMenuModule,
+    DialogModule,
+    FileUploadModule,
+    InputTextareaModule,
+    GrowlModule,
+    PanelModule,
+    TooltipModule
+  ],
+  declarations: []
+})
+export class PrimeNgModule { }
